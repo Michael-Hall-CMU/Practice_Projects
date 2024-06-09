51f69265d75a4e13a661fdd9c322e20a0542585a
mibyhall <mibyhall@gmail.com>
2024-06-09 01:08:10 -0500

    added some exception and error handling examples

---
+++ day-30-exceptions-and-errors/main.py
@@ -0,0 +1,26 @@
+# # FileNotFound
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+#     # print(a_dictionary["asdf"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error that I made up")
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
+
